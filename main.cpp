64fd045c7a4465ed6a59a93e4b276b46d1b6792d
Juerg Haefliger <juergh@proton.me>
2022-06-24 18:48:07 +0200

    Add -t, --timeout <seconds> commandline argument
    Signed-off-by: Juerg Haefliger <juergh@proton.me>

--- main.cpp
+++ main.cpp
@@ -5,13 +5,29 @@
  */
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 #include "qplayer.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	QPlayer qplayer;
+	QCommandLineParser parser;
+	QCommandLineOption timeoutOption(QStringList() << "t" << "timeout",
+		QApplication::translate("main", "Timeout."),
+		QApplication::translate("main", "seconds"));
+
+	parser.addHelpOption();
+	parser.addOption(timeoutOption);
+	parser.process(app);
+
+	int timeout = parser.value(timeoutOption).toInt();
+	if (timeout < 1) {
+		qDebug().nospace() << "Invalid timeout value: " << timeout;
+		return -1;
+	}
+
+	QPlayer qplayer(nullptr, timeout);
 	qplayer.show();
 	return app.exec();
 }
